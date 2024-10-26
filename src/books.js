6d97ea1043cfdeb43f8b6f635f5c93b12db8e854
Mina-Shah <shahmina814@gmail.com>
2024-10-26 10:52:40 +0500

    first commit

---
+++ src/books.js
@@ -0,0 +1,17 @@
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpeg";
+
+export const books = [
+  {
+    author: "Jordan Moore",
+    title: "Interesting Facts For Curious Minds",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: img2,
+    id: 2,
+  },
+];
